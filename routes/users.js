246001873aceeb1c42a3fa21391484bf6a3a8da4
KevinLarios143-01 <20002812@galileo.edu>
2021-08-24 19:18:58 -0600

    first commit

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,31 @@
+const { Router } = require('express');
 const express=require('express');
 const router=express.Router();
 
 router.get('/users/signin', (req, res)=>{
     res.render('users/signin');
+});
+
+router.get('/users/signup', (req, res)=>{
+    res.render('users/signup');
+});
+
+router.post('/users/signup', (req,res) => {
+    const {name,email,password,confirmpassword} = req.body;
+    const errors=[];
+
+    if(password != confirmpassword){
+        errors.push({text: 'Las claves no coinciden'});
+    }
+    if(password.length > 5){
+        errors.push({text: 'Su Clave es debil'});
+    }
+    if(errors.length >0){
+        res.render('users/signup', {errors,name, email, password, confirmpassword});
+    }else{
+        res.send('OK!');
+    }
+
 });
 
 
